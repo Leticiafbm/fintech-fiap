e8f74045a6f67e31c5cd5b99f60089ed09a9c3c1
Letícia <leticiafernandabritoeee@gmail.com>
2025-06-13 00:27:06 -0300

    FEAT: Mobile menu done

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
-import bootstrap from 'bootstrap';
+const mobileNavBar = document.getElementById("mobileNavBar");
+const openMenuButton = document.getElementById("openMenuButton");
+const closeMenuButton = document.getElementById("closeMenuButton");
+
+openMenuButton.addEventListener("click", ((e) => {
+  e.stopPropagation();
+
+  mobileNavBar.classList.add("active");
+  document.body.classList.add("menu-open");
+}));
+
+closeMenuButton.addEventListener("click", ((e) => {
+  e.stopPropagation();
+
+  mobileNavBar.classList.remove("active");
+  document.body.classList.remove("menu-open");
+}));
